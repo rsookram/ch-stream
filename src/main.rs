956b9d3cff7369938a86d9f0abd29c295ec66943
rsookram <rashadsookram@gmail.com>
2023-05-21 20:37:06 -0400

    Add support for output configuration
    through command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,88 @@
 use std::{
+    env::args,
     io::{BufWriter, Write},
     process::ExitCode,
 };
 
-fn main() -> ExitCode {
-    let first = 'a' as u32;
-    let last = 'z' as u32; // inclusive
-    assert!(last > first);
+struct Args {
+    first: char,
+    last_inclusive: char,
+    newline_prob: u8,
+}
 
-    let newline_prob = 10; // percentage
+fn main() -> ExitCode {
+    let mut args = args();
+    args.next();
+
+    let Args {
+        first,
+        last_inclusive,
+        newline_prob,
+    } = match parse_args(args) {
+        Ok(args) => args,
+        Err(err) => {
+            eprintln!("{}", err);
+            return ExitCode::from(2);
+        }
+    };
 
     let out = std::io::stdout().lock();
     let mut out = BufWriter::new(out);
 
     loop {
-        let result = write!(out, "{}", next_char(first, last, newline_prob));
+        let result = write!(out, "{}", next_char(first, last_inclusive, newline_prob));
 
         if let Err(err) = result {
             return if err.kind() == std::io::ErrorKind::BrokenPipe {
                 ExitCode::from(0)
             } else {
+                eprintln!("{}", err);
                 ExitCode::from(1)
             };
         }
     }
 }
 
-fn next_char(first: u32, last: u32, newline_prob: u8) -> char {
+fn parse_args(mut args: impl Iterator<Item = String>) -> Result<Args, String> {
+    let first_arg = args.next().ok_or("first character of range not provided")?;
+    let first: char = first_arg
+        .parse()
+        .map_err(|_| format!("first argument was `{first_arg}`, which is not a char"))?;
+
+    let second_arg = args
+        .next()
+        .ok_or("second character of range not provided")?;
+    let last_inclusive: char = second_arg
+        .parse()
+        .map_err(|_| format!("second argument was `{second_arg}`, which is not a char"))?;
+
+    if last_inclusive <= first {
+        return Err(format!(
+            "end of range must be >= start, given {first}..={last_inclusive}"
+        ));
+    }
+
+    let third_arg = args.next().ok_or("newline probability not provided")?;
+    let newline_prob: u8 = third_arg
+        .parse()
+        .map_err(|_| format!("third argument was `{third_arg}`, which is not a number"))?;
+    if !(0..=100).contains(&newline_prob) {
+        return Err(format!(
+            "newline probability must be given as a percentage (0..=100), given {third_arg}"
+        ));
+    }
+
+    Ok(Args {
+        first,
+        last_inclusive,
+        newline_prob,
+    })
+}
+
+fn next_char(first: char, last: char, newline_prob: u8) -> char {
     if fastrand::u8(0..100) < newline_prob {
         '\n'
     } else {
-        char::from_u32(fastrand::u32(first..=last)).unwrap()
+        char::from_u32(fastrand::u32(first as u32..=last as u32)).unwrap()
     }
 }
